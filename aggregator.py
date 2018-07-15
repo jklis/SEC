eeeefe461876e11cfab282c2bd68e0128442d320
jklis <jmklis33@gmail.com>
2018-07-14 21:46:51 -0400

    Writing all values to files from Yahoo

--- aggregator.py
+++ aggregator.py
@@ -1,6 +1,17 @@
 import csv
-from netIncome import get_past_net_income
 from yahoo import TickerValues
+
+# Net Income
+# Total Assets
+# Total Cash flow from operating activities
+# Long-term debt
+# Common Stock Equity
+# Preferred Stock Equity
+# Total Liabilities
+# Sale Purchase of stock
+# Gross Profit
+# Total Revenue
+
 
 def get_ticker_list(file, ticker_column, price_column, has_header=True):
     tickers = []
@@ -17,13 +28,31 @@ def get_ticker_list(file, ticker_column, price_column, has_header=True):
     return tickers, prices
 
 
+file_name = "output.csv"
+file = open(file_name, 'w')
+file_header = "Ticker,Year,Net Income,Total Assets,Total Cash Flow,Long-term Debt,Common Stock Equity,Preferred Stock Equity,Total Liabilities,Sale Purchase of Stock,Gross Profit,Total Revenue\n"
+file.write(file_header)
+
+file.close()
 tickers, prices = get_ticker_list("companylist.csv", 0, 2)
-net_incomes = []
-for ticker in tickers:
-    tick_value = TickerValues(ticker)
-    tick_value.update_all_values()
-    income = tick_value.net_income
-    print(income)
-    net_incomes.append(income)
-for income in net_incomes:
-    print(income)
+with open(file_name, 'a') as file:
+    for ticker in tickers:
+        print(ticker)
+        tick_value = TickerValues(ticker)
+        tick_value.update_all_values()
+        income = tick_value.net_income
+        total_assets = tick_value.total_assets
+        total_cash = tick_value.total_cash_flow
+        long_term_debt = tick_value.long_term_debt
+        common_stock = tick_value.common_stock_equity
+        preferred_stock = tick_value.preferred_stock_equity
+        total_liabilities = tick_value.total_liabilities
+        sale_purchase = tick_value.sale_purchase_stock
+        gross_profit = tick_value.gross_profit
+        total_revenue = tick_value.total_revenue
+        years = income.keys()
+        for year in years:
+            data = str.format("{},{},{},{},{},{},{},{},{},{},{},{}",ticker, year, income[year], total_assets[year],
+                              total_cash[year], long_term_debt[year], common_stock[year], preferred_stock[year],
+                              total_liabilities[year], sale_purchase[year], gross_profit[year], total_revenue[year])
+            file.write(data)
--- yahoo.py
+++ yahoo.py
@@ -30,17 +30,20 @@ class TickerValues:
         self.ticker = ticker
 
     def update_all_values(self):
-        income, balance, cash, years = self.get_tables()
-        self.net_income = self.get_value(income, years, "Net Income")
-        self.total_assets = self.get_value(balance, years, "Total Current Assets")
-        self.total_cash_flow = self.get_value(cash, years, "Total Cash Flow From Operating Activities")
-        self.long_term_debt = self.get_value(balance, years, "Long Term Debt")
-        self.common_stock_equity = self.get_value(balance, years, "Common Stock")
-        self.preferred_stock_equity = self.get_value(balance, years, "Preferred Stock")
-        self.total_liabilities = self.get_value(balance, years, "Total Liabilities")
-        self.sale_purchase_stock = self.get_value(cash, years, "Sale Purchase of Stock")
-        self.gross_profit = self.get_value(income, years, "Gross Profit")
-        self.total_revenue = self.get_value(income, years, "Total Revenue")
+        try:
+            income, balance, cash, years = self.get_tables()
+            self.net_income = self.get_value(income, years, "Net Income")
+            self.total_assets = self.get_value(balance, years, "Total Current Assets")
+            self.total_cash_flow = self.get_value(cash, years, "Total Cash Flow From Operating Activities")
+            self.long_term_debt = self.get_value(balance, years, "Long Term Debt")
+            self.common_stock_equity = self.get_value(balance, years, "Common Stock")
+            self.preferred_stock_equity = self.get_value(balance, years, "Preferred Stock")
+            self.total_liabilities = self.get_value(balance, years, "Total Liabilities")
+            self.sale_purchase_stock = self.get_value(cash, years, "Sale Purchase of Stock")
+            self.gross_profit = self.get_value(income, years, "Gross Profit")
+            self.total_revenue = self.get_value(income, years, "Total Revenue")
+        except TypeError:
+            return None
 
     def get_value(self, doc, years, value):
         output = {}
@@ -50,19 +53,22 @@ class TickerValues:
         return output
 
     def get_tables(self):
-        income_statement_url = "/financials?p="
-        balance_sheet_url = "/balance-sheet?p="
-        cash_flow_url = "/cash-flow?p="
-        income_statement_soup = self.get_soup(income_statement_url)
-        balance_sheet_soup = self.get_soup(balance_sheet_url)
-        cash_flow_soup = self.get_soup(cash_flow_url)
-        income_statement_table = income_statement_soup.find('table')
-        income_statement, years = self.parse_soup_table(income_statement_table)
-        cash_flow_table = cash_flow_soup.find('table')
-        cash_flow, years = self.parse_soup_table(cash_flow_table)
-        balance_sheet_table = balance_sheet_soup.find('table')
-        balance_sheet, years = self.parse_soup_table(balance_sheet_table)
-        return income_statement, balance_sheet, cash_flow, years
+        try:
+            income_statement_url = "/financials?p="
+            balance_sheet_url = "/balance-sheet?p="
+            cash_flow_url = "/cash-flow?p="
+            income_statement_soup = self.get_soup(income_statement_url)
+            balance_sheet_soup = self.get_soup(balance_sheet_url)
+            cash_flow_soup = self.get_soup(cash_flow_url)
+            income_statement_table = income_statement_soup.find('table')
+            income_statement, years = self.parse_soup_table(income_statement_table)
+            cash_flow_table = cash_flow_soup.find('table')
+            cash_flow, years = self.parse_soup_table(cash_flow_table)
+            balance_sheet_table = balance_sheet_soup.find('table')
+            balance_sheet, years = self.parse_soup_table(balance_sheet_table)
+            return income_statement, balance_sheet, cash_flow, years
+        except TypeError:
+            return None
 
     def get_soup(self, tab_url):
         base_url = "https://finance.yahoo.com/quote/"
@@ -72,30 +78,33 @@ class TickerValues:
         return soup
 
     def parse_soup_table(self, table):
-        rows = table.find_all('tr')
-        header = 0
-        years = []
-        output = {}
-        for row in rows:
-            columns = row.find_all('td')
-            if header == 0:
-                col = iter(columns)
-                next(col)
-                for column in col:
-                    years.append(column.get_text())
-                header += 1
-            elif len(columns) > 1:
-                index = 0
-                row_key = ""
-                for column in columns:
-                    if index == 0:
-                        row_key = column.get_text()
-                    else:
-                        output[(row_key, years[index - 1])] = column.get_text()
-                    index += 1
-        return output, years
+        try:
+            rows = table.find_all('tr')
+            header = 0
+            years = []
+            output = {}
+            for row in rows:
+                columns = row.find_all('td')
+                if header == 0:
+                    col = iter(columns)
+                    next(col)
+                    for column in col:
+                        years.append(column.get_text())
+                    header += 1
+                elif len(columns) > 1:
+                    index = 0
+                    row_key = ""
+                    for column in columns:
+                        if index == 0:
+                            row_key = column.get_text()
+                        else:
+                            output[(row_key, years[index - 1])] = column.get_text()
+                        index += 1
+            return output, years
+        except AttributeError:
+            return None
 
-
-aapl = TickerValues("aapl")
-aapl.update_all_values()
-print(aapl.net_income)
+#ticker = "ABEOW"
+#ticker_value = TickerValues(ticker)
+#ticker_value.update_all_values
+#print(ticker_value.net_income)
